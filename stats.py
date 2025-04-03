142bfa006e2851593d6bfaf8c991ffc643741b10
ivysauur <tanya.dunn1992@gmail.com>
2025-04-03 15:40:41 +0100

    add letter count to stats

--- stats.py
+++ stats.py
@@ -9,3 +9,12 @@ def get_num_words():
         num_words=len(words)
         print(f"{num_words} words found in the document")
 #get count of words
+def letter_count(book_text):
+	lower_words=book_text.lower()
+	letter_dic={}
+	for letters in lower_words:
+		if letters in letter_dic:
+			letter_dic[letters]+=1
+		else:
+			letter_dic[letters]=1
+	return letter_dic
