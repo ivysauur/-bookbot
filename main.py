dc0ac0b402573ad0c829eae4fb8a84feded6950f
ivysauur <tanya.dunn1992@gmail.com>
2025-04-04 14:52:44 +0100

    print report

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import get_book_text, get_num_words, letter_count
+from stats import get_book_text, get_num_words, letter_count, sort_on
 
 def main():
 	text = get_book_text("books/frankenstein.txt")
 	
 	num_words = get_num_words (text)
-	print(f"{num_words} words found in the document")
 
-	char_count = letter_count(text)
-	print(char_count)
-	
+	letter_dic = letter_count(text)
+	letter_order = sort_on(letter_dic)
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {num_words} total words")
+	print("--------- Character Count -------")
+	for char_data in letter_order:
+		char=char_data["char"]
+		if char.isalpha():
+			print(f"{char}: {char_data['count']}")
+	print("============= END ===============")
 
 if __name__ == "__main__":
 	main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+letter_dic={}
+
 def get_book_text(path_to_file):
         with open(path_to_file) as f:
                 file_contents = f.read()
@@ -10,10 +12,17 @@ def get_num_words(book):
 #get count of words
 def letter_count(book):
 	lower_words=book.lower()
-	letter_dic={}
 	for letters in lower_words:
 		if letters in letter_dic:
 			letter_dic[letters]+=1
 		else:
 			letter_dic[letters]=1
 	return letter_dic
+#get count of letters
+def sort_on(dict):
+	char_list = [{'char': letter, 'count': number} for letter, number in dict.items()]
+	char_list.sort(reverse=True,  key=lambda x: x['count'])
+	return char_list
+#order count of letters highest to lowest
+
+
